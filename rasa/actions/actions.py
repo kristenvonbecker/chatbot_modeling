85180727ad964d72e1d80da915dee71dbcf595ed
kristenvonbecker <71301766+kristenvonbecker@users.noreply.github.com>
2023-03-13 08:38:32 -0700

    Fix explanation rules and custom actions

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,86 +1,160 @@
-import json
+import logging
 
-from rasa_sdk.events import SlotSet
-from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk.events import SlotSet, SessionStarted, ActionExecuted, BotUttered
+from rasa_sdk import Action, Tracker
+from rasa_sdk.forms import ValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 
 from typing import Any, Text, Dict, List
 import os
+import json
 import pandas as pd
-from fuzzywuzzy import fuzz
+import ast
+from fuzzywuzzy import fuzz, process
+from nltk.tokenize import sent_tokenize
 
 from dotenv import load_dotenv
-load_dotenv();
+load_dotenv()
 
-# fuzzy matching threshold (max = 100)
-T = 80
+logger = logging.getLogger(__name__)
+
+
+# import data needed for chatbot responses
 
 home = os.getenv("PROJ_HOME")
-data_filepath = os.path.join(home, "explorer_ai/data/adv_data.json")
+exhibits_filepath = os.path.join(home, "data/chatbot_knowledgebase/institutional/exhibits.json")
+galleries_filepath = os.path.join(home, "data/chatbot_knowledgebase/institutional/galleries.json")
+articles_filepath = os.path.join(home, "data/chatbot_knowledgebase/subject_matter/encyclopedia_britannica.csv")
 
-with open(data_filepath, "r") as infile:
-    data_dict = json.load(infile)
+with open(exhibits_filepath, "r") as f:
+    exhibits = json.load(f)
 
-df = pd.DataFrame(data_dict)
+with open(galleries_filepath, "r") as f:
+    galleries = json.load(f)
 
-df_titles = df["title"]
-df_alt_titles = df["alt_titles"]
-df_field = df["field"]
-df_person = df["is_person"]
-df_text = df["text"]
+articles = pd.read_csv(articles_filepath,
+                       header=0,
+                       names=["Id", "Title", "Alt Title", "Field", "Is Person", "Text"],
+                       index_col=0)
 
-titles_list = []
-for main_title, alt_titles in zip(df_titles, df_alt_titles):
-    these_titles = [main_title] + alt_titles
-    titles_list.append(these_titles)
+ids = articles.index.tolist()
+titles = articles["Title"]
+alt_titles = articles["Alt Title"].apply(lambda x: ast.literal_eval(x))
+field = articles["Field"].apply(lambda x: ast.literal_eval(x))
+is_person = articles["Is Person"]
+text = articles["Text"]
 
-df = pd.DataFrame({"titles": titles_list, "field": df_field, "is_person": df_person, "text": df_text})
+alias_id_lookup = {}
+title_aliases = []
+for article_id, main_title, alt_titles in zip(ids, titles, alt_titles):
+    aliases = [main_title] + alt_titles
+    alias_id_lookup.update({article_id: aliases})
+    title_aliases += aliases
 
 
-def is_match(text1, text2, threshold):
-    return fuzz.token_sort_ratio(text1, text2) >= threshold
+text_id_lookup = {}
+for article_id, text in zip(ids, text):
+    text_id_lookup.update({article_id: text})
 
 
-class ValidateExplanationForm(FormValidationAction):
+def get_title_matches(subject, threshold=90, scorer=fuzz.WRatio):
+    matches = process.extract(subject, title_aliases, scorer=scorer, limit=5)
+    matches.sort(key=lambda item: item[1], reverse=True)
+    good_matches = [match for (match, score) in matches if score >= threshold]
+    return good_matches
+
+
+def get_text(article_id, num_sent=3):
+    full_text = text_id_lookup[article_id]
+    sentences = sent_tokenize(full_text)
+    txt = " ".join(sentences[:num_sent])
+    return txt
+
+
+class ActionSessionStart(Action):
     def name(self) -> Text:
-        return "validate_explanation_form"
+        return "action_session_start"
 
-    def validate_subject(
-            self,
-            slot_value: Any,
+    async def run(self,
+                  dispatcher: CollectingDispatcher,
+                  tracker: Tracker,
+                  domain: DomainDict
+                  ) -> List[Dict[Text, Any]]:
+        intro = domain["responses"]["utter_intro"][0]["text"]
+        what_i_do = domain["responses"]["utter_what_i_do"][0]["text"]
+        how_help = domain["responses"]["utter_how_help"][0]["text"]
+        events = [
+            SessionStarted(),
+            BotUttered(text=intro),
+            BotUttered(text=what_i_do),
+            BotUttered(text=how_help),
+            ActionExecuted("action_listen")
+        ]
+        return events
+
+
+class ActionGetMatchIds(Action):
+    def name(self) -> Text:
+        return "action_get_match_ids"
+
+    def run(self,
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
-            domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        matches = []
-        for titles in titles_list:
-            for title in titles:
-                if (score := fuzz.token_sort_ratio(slot_value, title)) >= T:
-                    matches.append((titles[0], score))
-        if matches:
-            matches.sort(key=lambda i: i[1], reverse=True)
-            return {"subject": matches[0][0]}
-        else:
-            dispatcher.utter_message(response="utter_ask_about_other")
-            return {"subject": None}
-
-
-class ActionGetResults(Action):
-    def name(self) -> Text:
-        return "action_get_results"
-
-    def run(
-            self,
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: DomainDict,
-    ) -> List[Dict[Text, Any]]:
+            domain: DomainDict) -> List[Dict[Text, Any]]:
         subject = tracker.get_slot("subject")
-        mask = df["titles"].apply(lambda x: any(is_match(y, subject, T) for y in x))
-        results = df["text"][mask].tolist()
+        matches = []
+        if subject:
+            matches += get_title_matches(subject)
+        match_ids = []
+        for match in matches:
+            for article_id, aliases in alias_id_lookup.items():
+                if match in aliases:
+                    match_ids.append(article_id)
+        match_ids = list(set(match_ids))
+        matches_available = True if match_ids else False
+        return [
+            SlotSet("match_ids", match_ids),
+            SlotSet("matches_available", matches_available)
+        ]
 
-        return [SlotSet("match", results[0] if results is not None else "")]
+
+class ActionGiveExplanation(Action):
+    def name(self) -> Text:
+        return "action_give_explanation"
+
+    def run(self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict
+            ) -> List[Dict[Text, Any]]:
+        subject = tracker.get_slot("subject")
+        match_ids = tracker.get_slot("match_ids")
+        matches_available = tracker.get_slot("matches_available")
+        num_tries = int(tracker.get_slot("num_tries"))
+        if not subject:
+            return []
+        if matches_available:
+            explanation = get_text(match_ids[num_tries], num_sent=3)
+            if num_tries == 0:
+                dispatcher.utter_message(response="utter_found_something")
+            else:
+                dispatcher.utter_message(response="utter_found_something_else")
+            dispatcher.utter_message(text=explanation)
+            num_tries += 1
+            matches_available = (len(match_ids) > num_tries)
+            return [
+                SlotSet("subject", subject),
+                SlotSet("num_tries", num_tries),
+                SlotSet("matches_available", matches_available)
+            ]
+        else:
+            dispatcher.utter_message(response="utter_found_nothing_else")
+            return [
+                SlotSet("subject", None),
+                SlotSet("match_ids", []),
+                SlotSet("num_tries", 0)
+            ]
 
 
 class ActionResetSubject(Action):
@@ -89,3 +163,27 @@ class ActionResetSubject(Action):
 
     def run(self, dispatcher, tracker, domain) -> List[Dict[Text, Any]]:
         return [SlotSet("subject", None)]
+
+
+class ActionResetMatchIds(Action):
+    def name(self) -> Text:
+        return "action_reset_match_ids"
+
+    def run(self, dispatcher, tracker, domain) -> List[Dict[Text, Any]]:
+        return [SlotSet("match_ids", [])]
+
+
+class ActionResetMatchesAvailable(Action):
+    def name(self) -> Text:
+        return "action_reset_matches_available"
+
+    def run(self, dispatcher, tracker, domain) -> List[Dict[Text, Any]]:
+        return [SlotSet("matches_available", False)]
+
+
+class ActionResetNumTries(Action):
+    def name(self) -> Text:
+        return "action_reset_num_tries"
+
+    def run(self, dispatcher, tracker, domain) -> List[Dict[Text, Any]]:
+        return [SlotSet("num_tries", 0)]
